7fa14fc51e92ca26f120935a56031a43f72f72c8
HUGH BRADY - Student <G00338260@gmit.ie>
2017-09-20 10:45:13 +0100

    attempt one

---
+++ 02-timedate.go
@@ -0,0 +1,15 @@
+// author: Hugh Brady
+// Date 2017-09-20
+
+// Adapted from https://tour.golang.org/welcome/4
+
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	fmt.Println("The time is", time.Now())
+}
