70c475c3523bac8971dd1158ccc773b4ea18a51d
Hugh <Hugh>
2017-09-27 22:12:34 +0100

    Added problem 1 solution

---
+++ 01-hellojapanese.go
@@ -0,0 +1,12 @@
+// Go Problem 1 - Kon'ichiwa, Sekai
+// Hugh Brady - G00338260 - 27/09/17
+
+// Code adapted from https://tour.golang.org/welcome/1
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, 世界")
+}
