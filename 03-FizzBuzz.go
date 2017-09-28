8fcda4f1f0d9a2a2f7565a973409e889ebf0e542
Hugh <Hugh>
2017-09-28 13:56:29 +0100

    Got FizzBuzz working, Fizz and Buzz to finish

--- 03-FizzBuzz.go
+++ 03-FizzBuzz.go
@@ -3,15 +3,25 @@
 
 package main
 
-import {
+import (
 	"fmt"
-}
+)
 
 func main() {
-	num := 0
+	var i int = 0
 	
-	for i :=; i <= 100; i++
-	{
+	// for loop running through numbers 1 - 100
+	for i < 100{
+		i++
+		
+		// check if i is divisible by 3 or 5
+		if i%3 == 0 && i%5 == 0{
+			fmt.Printf("FizzBuzz\n")
+		} else {
+			fmt.Printf("%d\n", i)
+		}// end if else
+			
+		
 		
 	}
 	
